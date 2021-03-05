1f914f8510c5923bf4b701c9ee9fd2cb09cf85a1
MaschaU <masa.utkovic@gmail.com>
2021-03-05 12:36:27 +0100

    cleaned url and add company logo

--- src/JobPostingDetails.jsx
+++ src/JobPostingDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import { JobsContext } from "./JobsContext";
 
--- src/JobPostings.jsx
+++ src/JobPostings.jsx
@@ -1,10 +1,12 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext } from "react";
 import { JobsContext } from "./JobsContext";
 import { Link } from 'react-router-dom'
 
 const JobPostings = ({match}) => {
     const { data: {jobs}} = useContext(JobsContext);
-    
+    const shortenedUrl = (url) => {
+        return url = url && url.replace(/^(?:https?:\/\/)?(?:www\.)?/i, "").split('/')[0];
+    }
 
     return(
         jobs.map((job) => (
@@ -14,12 +16,12 @@ const JobPostings = ({match}) => {
                 <div className="job__listing">
                 <div className="job__listing__header">
                     <div className="header__image__container">
-                        {job.company_logo}
+                    {job.company_logo && (<img src={job.company_logo} alt={`${job.company} company logo`} />)}
                     </div>
                     <div className="header__textbox">
                         <div className="textbox__inner">
                             <h1 className="header__textbox__heading">{job.company}</h1>
-                            <p className="header__textbox__site">{job.company_url}</p>
+                            <p className="header__textbox__site">{shortenedUrl(job.company_url)}</p>
                         </div>
                         <div className="header__textbox__company-redirect" >
                             {job.company_url}
