aba3ba428ea0322639a06b0b0eac841466bd7a56
MaschaU <masa.utkovic@gmail.com>
2021-04-10 16:28:22 +0200

    clear commented out code

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,37 +1,11 @@
 import React from "react";
-import { useEffect } from "react";
 import { useJobsContext } from "../JobsContext";
-import { bgPatternHeader, iconMoon, iconSun, logo } from "../visuals/desktop";
+import { bgPatternHeader, logo } from "../visuals/desktop";
 import Toggle from "../Toggle/Toggle";
 
 import "./Header.scss";
 
 const Header = () => {
-  // const htmlTag = document.body.parentElement;
-  const { defaultTheme, setDefaultTheme } = useJobsContext();
-
-  /*useEffect(() => {
-    if (!localStorage.getItem("themes")) {
-      setDefaultTheme(true);
-      localStorage.setItem("themes", "defaultTheme");
-      htmlTag.setAttribute("data-theme", "defaultTheme");
-    } else {
-      htmlTag.setAttribute("data-theme", localStorage.getItem("themes"));
-    }
-  }, [htmlTag, setDefaultTheme]);
-
-  /*const switchTheme = () => {
-    if (!defaultTheme) {
-      localStorage.setItem("themes", "defaultTheme");
-      htmlTag.setAttribute("data-theme", localStorage.getItem("themes"));
-      setDefaultTheme(true);
-    } else {
-      localStorage.setItem("themes", "dark");
-      htmlTag.setAttribute("data-theme", localStorage.getItem("themes"));
-      setDefaultTheme(false);
-    }
-  };*/
-
   return (
     <div className="header__desktop">
       <div className="header__backgrounds">
--- src/Toggle/Toggle.jsx
+++ src/Toggle/Toggle.jsx
@@ -10,49 +10,45 @@ const Toggle = () => {
 	const htmlTag = document.body.parentElement;
 
 	useEffect(() => {
-    if (!localStorage.getItem("themes")) {
-      setDefaultTheme(true);
-      localStorage.setItem("themes", "defaultTheme");
-      htmlTag.setAttribute("data-theme", "defaultTheme");
-    } else {
-      htmlTag.setAttribute("data-theme", localStorage.getItem("themes"));
-    }
-  }, [htmlTag, setDefaultTheme]);
+		if (!localStorage.getItem("themes")) {
+			setDefaultTheme(true);
+			localStorage.setItem("themes", "defaultTheme");
+			htmlTag.setAttribute("data-theme", "defaultTheme");
+		} else {
+			htmlTag.setAttribute("data-theme", localStorage.getItem("themes"));
+		}
+	}, [htmlTag, setDefaultTheme]);
 
 	const switchTheme = () => {
-    if (!defaultTheme) {
-      localStorage.setItem("themes", "defaultTheme");
-      htmlTag.setAttribute("data-theme", localStorage.getItem("themes"));
-      setDefaultTheme(true);
-    } else {
-      localStorage.setItem("themes", "dark");
-      htmlTag.setAttribute("data-theme", localStorage.getItem("themes"));
-      setDefaultTheme(false);
-    }
-  };
-    return (
-			
-      <div class="theme-switch">
-        
-					<span>
-            <img className="theme-switch__logo" src={iconSun} alt="sun" />
-          </span>
-					<div class="toggle toggle__modes">
-						<input type="checkbox" id="toggle__modes" 
-						className={`toggle__checkbox ${!defaultTheme ? "active" : ""}`}
-						onClick={() => switchTheme()}/>
-						<label class="toggle__btn" for="toggle__modes">
-						<span class="toggle__feature"></span>
-						</label>
-					</div>
-					<span>
-            <img className="theme-switch__logo" src={iconMoon} alt="moon" />
-          </span>
-          
-        
-      </div>
-			
-    )
+		if (!defaultTheme) {
+			localStorage.setItem("themes", "defaultTheme");
+			htmlTag.setAttribute("data-theme", localStorage.getItem("themes"));
+			setDefaultTheme(true);
+		} else {
+			localStorage.setItem("themes", "dark");
+			htmlTag.setAttribute("data-theme", localStorage.getItem("themes"));
+			setDefaultTheme(false);
+		}
+	};
+	return (
+		<div class="theme-switch">
+			<span>
+				<img className="theme-switch__logo" src={iconSun} alt="sun" />
+			</span>
+			<div class="toggle toggle__modes">
+				<input type="checkbox" id="toggle__modes"
+					className={`toggle__checkbox ${!defaultTheme ? "active" : ""}`}
+					onClick={() => switchTheme()} />
+				<label class="toggle__btn" for="toggle__modes">
+					<span class="toggle__feature"></span>
+				</label>
+			</div>
+			<span>
+				<img className="theme-switch__logo" src={iconMoon} alt="moon" />
+			</span>
+		</div>
+
+	)
 }
 
 export default Toggle;
